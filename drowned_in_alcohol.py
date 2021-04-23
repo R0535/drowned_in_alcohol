bb2f069b3f174b4ae1a4e4e38ddad9c67588c993
R0535 <jhernandez@bcodemexico.com>
2021-04-22 19:25:52 -0500

    los olvidados

--- drowned_in_alcohol.py
+++ drowned_in_alcohol.py
@@ -1,11 +1,46 @@
 import time
 import os
-
+import random as random
 clear = lambda :os.system('cls')
 
 def run():
     showintro()
 #UI
+def showroundwinner(nombre):
+    print("""
+   *************************************************************************************************************
+        **************************************{} ha ganado este round**************************************
+   *************************************************************************************************************
+
+    """.format(nombre))
+    input("                                          Presione Enter para continuar")
+
+
+def showgamewinner(nombre):
+    print("""
+   *************************************************************************************************************
+            *********************************{} ha ganado esta partida*********************************
+             **************************************El premio es:**************************************
+        **************************************Un fondo, un shot y un reto!***********************************
+   *************************************************************************************************************
+
+    """.format(nombre))
+    input("                                          Presione Enter para continuar")
+    showintro()
+
+def printdrink():
+    print("""
+                     ██████████   ███████████   █████ ██████   █████ █████   ████ ███
+                    ░░███░░░░███ ░░███░░░░░███ ░░███ ░░██████ ░░███ ░░███   ███░ ░███
+                     ░███   ░░███ ░███    ░███  ░███  ░███░███ ░███  ░███  ███   ░███
+                     ░███    ░███ ░██████████   ░███  ░███░░███░███  ░███████    ░███
+                     ░███    ░███ ░███░░░░░███  ░███  ░███ ░░██████  ░███░░███   ░███
+                     ░███    ███  ░███    ░███  ░███  ░███  ░░█████  ░███ ░░███  ░░░ 
+                     ██████████   █████   █████ █████ █████  ░░█████ █████ ░░████ ███
+                    ░░░░░░░░░░   ░░░░░   ░░░░░ ░░░░░ ░░░░░    ░░░░░ ░░░░░   ░░░░ ░░░         
+    """)
+def reset_palabra_ui(palabra):
+    print(palabra)
 def showintro():
     showtitle()
     #time.sleep(3)
@@ -160,14 +195,101 @@ def showmenu():
     assert menu_option.isnumeric(), showerror()
     
     menumanager(menu_option)
+def showheader():
+    print("""
+         ██████╗ ███╗   ██╗███████╗                                                                        
+        ██╔═══██╗████╗  ██║██╔════╝                                                                        
+        ██║   ██║██╔██╗ ██║█████╗                                                                          
+        ██║   ██║██║╚██╗██║██╔══╝                                                                          
+        ╚██████╔╝██║ ╚████║███████╗                                                                        
+         ╚═════╝ ╚═╝  ╚═══╝╚══════╝                                                                        
 
+        ██████╗ ██████╗ ██╗███╗   ██╗██╗  ██╗██╗███╗   ██╗ ██████╗      ██████╗  █████╗ ███╗   ███╗███████╗
+        ██╔══██╗██╔══██╗██║████╗  ██║██║ ██╔╝██║████╗  ██║██╔════╝     ██╔════╝ ██╔══██╗████╗ ████║██╔════╝
+        ██║  ██║██████╔╝██║██╔██╗ ██║█████╔╝ ██║██╔██╗ ██║██║  ███╗    ██║  ███╗███████║██╔████╔██║█████╗  
+        ██║  ██║██╔══██╗██║██║╚██╗██║██╔═██╗ ██║██║╚██╗██║██║   ██║    ██║   ██║██╔══██║██║╚██╔╝██║██╔══╝  
+        ██████╔╝██║  ██║██║██║ ╚████║██║  ██╗██║██║ ╚████║╚██████╔╝    ╚██████╔╝██║  ██║██║ ╚═╝ ██║███████╗
+        ╚═════╝ ╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝ ╚═════╝      ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝
+    """)
+def showtable(players):
+    print("""
+        ###########################################################################################
+        ###########################################################################################
+        ##                #                               #                                      ##
+        ##       id       #            nombre             #             puntuacion               ##
+        ##                #                               #                                      ##
+        ###########################################################################################
+        ###########################################################################################        
+    """)    
+    for player in players:
+        print("""     
+                {}                      {}                                  {}                 
+        """.format(player["id"], player["nombre"], player["puntos"]))
 
 #GAME
 def startgame():
-    clear()
-    
+    clear()    
+    actual_player = ""
+    players = registerplayers()
+    for i in range(0,15):
+        clear()   
+        round_won=False
+        palabra = wordpicker()
+        new_palabra = []
+        counter = 0
+        for letra in palabra:
+            if(letra=="\n"):
+                continue
+            counter += 1
+            if(counter % 4 ==0):
+                new_palabra.append(letra)
+                continue
+            new_palabra.append("_")
+            turn_counter = 0
+        while (round_won != True):          
+            actual_player = players[turn_counter]
+            showheader()
+            showtable(players)
+            reset_palabra_ui(new_palabra)
+            letra_input = input("Turno de: {}. Letra: ".format(actual_player["nombre"]))
+            new_palabra = validatepalabra(palabra, new_palabra, letra_input)
+            round_won = not new_palabra.__contains__("_")
+            turn_counter+=1
+            clear()
+            if(turn_counter >=len(players)):
+                printdrink()            
+                turn_counter = 0
+        new_player = actual_player
+        new_player["puntos"] += 1     
+        players[players.index(actual_player)] = new_player
+        showroundwinner(actual_player["nombre"])
+    winner = setwinner(players)
+    showgamewinner(winner)
+        
+
 
 #MANAGERS
+def setwinner(players):
+    older = 0
+    actual_winner = ""
+    for player in players:
+        if (player["puntos"]>older):
+            actual_winner = player["id"]
+            older = player["puntos"]
+    nameplayer = players[actual_winner]
+    return nameplayer["nombre"]
+
+
+def validatepalabra(palabra, palabra_ui,letra):
+    result = palabra_ui
+    counter = 0
+    for indice in palabra:
+        if indice == letra:
+            result[counter] = (letra)  
+        counter += 1          
+    counter = 0
+    return result
+    
 def menumanager(menu_option):
     menu_option = int(menu_option)
     assert menu_option>=1 and menu_option<4, showerror()
@@ -181,7 +303,62 @@ def menumanager(menu_option):
         print("Error Inexplicable")
         showmenu()  
 def wordpicker():
+    with open("data.txt", "r",encoding="utf-8") as f:
+        palabras=[palabra for palabra in f if palabra!=""]
+        linea_random = random.randint(1,len(palabras))
+        palabra = palabras[linea_random]
+        return palabra
+def getplayer(i,nombres):
+    print("Jugadores Registrados--->>> ")
+    for nombre in nombres:
+        print(nombre)
+        print("\n")
+
+    nombre_input = input("Ingrese un nombre valido para el jugador: ")
+    if(nombre_input != '' and len(nombre_input) >=3):
+        nombres.append({
+            "id":i,
+            "nombre":nombre_input,
+            "puntos":0
+        })
+    else:
+        clear()
+        showheader()
+        nombres = getplayer(i,nombres)
+    return nombres
+
+
+
+def registerplayers():    
+    clear()
     
+    showheader() 
+    num_validated = False
+    jugadores_len = 0
+    while (num_validated != True):
+        clear()
+        showheader()
+        jugadores_len = input("¿Cuantos jugadores son?")
+        if(jugadores_len.isnumeric()):
+            num_validated = True
+        else:
+            clear()
+            showheader()
+            print("Ingresa un numero valido")
+            time.sleep(3)
+
+    nombres = []
+    for i in range(0,int(jugadores_len)):
+        clear()
+        showheader()
+        nombres = getplayer(i,nombres)
+    print("Jugadores Registrados--->>> ")
+
+    clear()
+    showheader()
+    showtable(nombres)
+    return nombres
+        
 
 
 if __name__ == "__main__":
